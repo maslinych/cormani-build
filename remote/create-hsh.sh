1a1fa67826d478bfdee712c883c6de3dd8b848e4
Kirill Maslinsky <kirill@altlinux.org>
2018-02-04 22:23:18 +0300

    update remote scripts (copy from corbama-build)

--- remote/create-hsh.sh
+++ remote/create-hsh.sh
@@ -10,13 +10,17 @@ fi
 chroot_user="$(stat -c %U $other_chroot/chroot/var/)"
 
 if [ "$chroot_user" == "corpora_a1" ] 
-then number_opt="--number=1"
-else number_opt=""
+then number_opt="--number=0"
+else number_opt="--number=1"
 fi
 
-hsh "$number_opt" --initroot --pkg-build-list="basesystem" --no-cache "$hasher_chroot"
-hsh-install "$number_opt" "$hasher_chroot" manatee-open bonito2-open apache2-base
+hsh "$number_opt" --initroot --pkg-build-list="basesystem" --no-cache "$hasher_chroot" ; \
+hsh-install "$number_opt" "$hasher_chroot" manatee-open bonito2-open apache2-base iproute2
+
 cp setup-corpus-environment.sh "$hasher_chroot/chroot/.in/"
 cp setup-bonito.sh "$hasher_chroot/chroot/.in/"
-share_ipc=yes share_network=yes hsh-run "$number_opt" --mount=/proc --rooter "$hasher_chroot" -- sh setup-corpus-environment.sh "$port"
 
+if test -z "$number_opt"
+then share_network=yes hsh-run --mount=/proc --rooter "$hasher_chroot" -- sh setup-corpus-environment.sh "$port"
+else share_network=yes hsh-run "$number_opt" --mount=/proc --rooter "$hasher_chroot" -- sh setup-corpus-environment.sh "$port"
+fi
--- remote/setup-bonito.sh
+++ remote/setup-bonito.sh
@@ -11,11 +11,9 @@ do
 done
 # setup apache dir
 cp /etc/httpd2/conf/sites-available/bonito2.conf /etc/httpd2/conf/sites-available/"$corpname".conf
-sed -i "/Alias/s/bonito2\?/$corpname/g" /etc/httpd2/conf/sites-available/"$corpname".conf
-sed -i "/Directory/s/bonito2\?/$corpname/g" /etc/httpd2/conf/sites-available/"$corpname".conf
+sed -i "s/bonito2\?/$corpname/g" /etc/httpd2/conf/sites-available/"$corpname".conf
 mkdir -p "$corpdir"
 a2ensite "$corpname"
-a2enmod cgi
 # setup bonito instance
 setupbonito "$corpdir" /var/lib/manatee
 cgifile="$corpdir/run.cgi"
